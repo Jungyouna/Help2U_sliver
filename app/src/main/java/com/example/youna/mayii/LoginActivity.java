5eaa98080d0bb0c6fd9918a8d2c62f3721374f3b
LAPTOP-A3I1LMCI\Youna <jyn1996@naver.com>
2018-06-06 03:35:47 +0900

    youna_message send

--- app/src/main/java/com/example/youna/mayii/LoginActivity.java
+++ app/src/main/java/com/example/youna/mayii/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.youna.mayii;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +11,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button5;
+    private EditText p_phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        p_phoneNumber = (EditText)findViewById(R.id.p_phoneNumber);
 
+        button5 = (Button)findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SilverActivity.class);
+                intent.putExtra("name", p_phoneNumber.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/youna/mayii/SilverActivity.java
+++ app/src/main/java/com/example/youna/mayii/SilverActivity.java
@@ -1,48 +1,66 @@
 package com.example.youna.mayii;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class SilverActivity extends AppCompatActivity {
-    BluetoothAdapter mBluetoothAdapter;
-    Button btnONOFF;
-    ImageView imageView4;
-    EditText phoneNumber, help;
+    private Button button;
+    private String help_message = "응급상황 입니다!";
+    private TextView textView3;
+    private TextView textView6;
+    private String name, phoneNo, sms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_silver);
-        phoneNumber = (EditText)findViewById(R.id.phoneNumber);
-        help = (EditText)findViewById(R.id.help);
-
-        imageView4.setOnClickListener(new View.OnClickListener() {
+        button = (Button)findViewById(R.id.button);
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView6 = (TextView)findViewById(R.id.textView6);
+        textView3.setText(name);
+        textView6.setText(help_message);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputNumber = phoneNumber.getText().toString();
-                String inputHelp = help.getText().toString();
-                if(inputNumber.length()>0 && inputHelp.length()>0) {
-                    //sendSMS(inputNumber, inputHelp); Toast.makeText(getBaseContext(), inputNumber+"\n"+inputHelp, Toast.LENGTH_SHORT).show();
+                Log.d("!!!",name);
+                phoneNo = name;
+                sms = help_message;
+                Toast.makeText(getApplicationContext(), "전송중...", Toast.LENGTH_LONG).show();
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "메세지 전송이 완료되었습니다", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "메세지 전송이 실패되었습니다." +
+                            "\n다시 시도해주세요!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
-                else
-                    Toast.makeText(getBaseContext(), "전화번호와 메시지를 입력해주세요.", Toast.LENGTH_SHORT).show();
-            }});
+
+            }
+        });
     }
-    /*private void sendSMS(String phoneNumber, String message)
-    {
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, null, null);
-    }*/
 }
